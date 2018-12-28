fd1510edf140bb6878a609d4f13431d1f2580ca6
Valentin Roland <valentin@vroland.de>
2018-12-28 14:57:12 +0100

    basic mfnf2serlo example

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#[cfg(feature = "mfnf")]
+mod mfnf;
 mod plugins;
 
 pub use crate::plugins::*;
 
 #[cfg(test)]
 mod test {
+    #[cfg(feature = "mfnf")]
+    use crate::mfnf::*;
     use crate::{Heading, Markdown, Plugins};
 
     fn example_heading_doc() -> Plugins {
@@ -56,5 +60,17 @@ mod test {
             "state": { "content": "Test" }
         }"#;
         let _: Markdown = serde_json::from_str(&doc).unwrap();
+    }
+
+    #[test]
+    #[cfg(feature = "mfnf")]
+    fn simple_mfnf_to_plugins() {
+        let root = serde_json::from_reader(
+            fs::File::open("src/mfnf_example.json").expect("could not read example!"),
+        )
+        .expect("could deserialize example!");
+        let plugins = plugins_from_element(root).expect("conversion error!");
+        let ser = serde_json::to_string_pretty(&plugins).expect("could not serialize!");
+        eprintln!("{}", &ser);
     }
 }
