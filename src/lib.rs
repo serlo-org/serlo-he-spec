86bfc8274dd5d5a336572358f24f10f15aaaf5dc
Valentin Roland <valentin@vroland.de>
2019-01-29 17:05:46 +0100

    better typed text attributes

--- src/lib.rs
+++ src/lib.rs
@@ -14,25 +14,29 @@ mod test {
     use uuid::Uuid;
 
     fn example_heading_doc() -> HEPluginInstance<Plugins> {
-        Plugins::Heading(Heading {
+        Plugins::HeHeading(HeHeading {
             id: Uuid::new_v4(),
-            caption: Title {
+            caption: HeTitle {
                 id: Uuid::new_v4(),
-                content: "Hello World".into(),
+                content: TitleText::from_str("Hello World"),
             }
             .into(),
-            content: vec![Plugins::Heading(Heading {
+            content: vec![Plugins::HeHeading(HeHeading {
                 id: Uuid::new_v4(),
-                caption: Title {
+                caption: HeTitle {
                     id: Uuid::new_v4(),
-                    content: "Subheading".into(),
-                }.into(),
-                content: vec![Plugins::Markdown(Markdown {
+                    content: TitleText::from_str("Subheading"),
+                }
+                .into(),
+                content: vec![Plugins::HeMarkdown(HeMarkdown {
                     id: Uuid::new_v4(),
-                    content: "Document content".into(),
-                }).into()],
-            }).into()],
-        }).into()
+                    content: MarkdownText::from_str("Document content"),
+                })
+                .into()],
+            })
+            .into()],
+        })
+        .into()
     }
     #[test]
     fn serialize_heading() {
@@ -43,8 +47,10 @@ mod test {
 
     #[test]
     fn serde_heading() {
-        let doc = serde_json::to_string(&example_heading_doc()).expect("serialization failed");
-        let tree: HEPluginInstance<Plugins> = serde_json::from_str(&doc).expect("could not deserialize");
+        let doc =
+            serde_json::to_string_pretty(&example_heading_doc()).expect("serialization failed");
+        let tree: HEPluginInstance<Plugins> =
+            serde_json::from_str(&doc).expect("could not deserialize");
         eprintln!(
             "{}",
             &serde_json::to_string_pretty(&tree).expect("could not serialize!")
@@ -54,19 +60,22 @@ mod test {
     #[test]
     fn deserialize_single_markdown() {
         let doc = r#"{
-          "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-          "cells": [
-            {
-              "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-              "content": {
-                "plugin": { "name": "@serlo-org/editor-plugin-he-markdown", "version": "0.1.0" },
-                "state": { "content": "Test" }
-              },
-              "rows": null
-            }
-          ]
+          "type": "@splish-me/editor-core/editable",
+          "state": {
+            "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+            "cells": [
+              {
+                "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+                "content": {
+                  "plugin": { "name": "@serlo/editor-plugin-he-markdown", "version": "0.1.0" },
+                  "state": { "content": "Test" }
+                },
+                "rows": null
+              }
+            ]
+          }
         }"#;
-        let _: HEPluginInstance<Markdown> = serde_json::from_str(&doc).unwrap();
+        let _: HEPluginInstance<HeMarkdown> = serde_json::from_str(&doc).unwrap();
     }
 
     #[test]
@@ -74,39 +83,44 @@ mod test {
     fn deserialize_wrong_plugin() {
         // state is in fact a markdown plugin -> should panic
         let doc = r#"{
-          "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-          "cells": [
-            {
-              "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-              "content": {
-                "plugin": { "name": "@serlo-org/editor-plugin-he-title", "version": "0.0.0" },
-                "state": { "content": "Test" }
-              },
-              "rows": null
-            }
-          ]
+          "type": "@splish-me/editor-core/editable",
+          "state": {
+            "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+            "cells": [
+              {
+                "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+                "content": {
+                  "plugin": { "name": "@serlo/editor-plugin-he-title", "version": "0.1.0" },
+                  "state": { "content": "Test" }
+                },
+                "rows": null
+              }
+            ]
+          }
         }"#;
-        let _: HEPluginInstance<Markdown> = serde_json::from_str(&doc).unwrap();
+        let _: HEPluginInstance<HeMarkdown> = serde_json::from_str(&doc).unwrap();
     }
 
     #[test]
     #[should_panic]
     fn deserialize_incompatible_version() {
-        // state is in fact a markdown plugin -> should panic
         let doc = r#"{
-          "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-          "cells": [
-            {
-              "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
-              "content": {
-                "plugin": { "name": "@serlo-org/editor-plugin-he-title", "version": "10000.0.0" },
-                "state": { "content": "Test" }
-              },
-              "rows": null
-            }
-          ]
+          "type": "@splish-me/editor-core/editable",
+          "state": {
+            "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+            "cells": [
+              {
+                "id": "a6f91bdc-403f-49d3-831d-5c0d09bfc28f",
+                "content": {
+                  "plugin": { "name": "@serlo/editor-plugin-he-title", "version": "10000.1.0" },
+                  "state": { "content": "Test" }
+                },
+                "rows": null
+              }
+            ]
+          }
         }"#;
-        let _: HEPluginInstance<Title> = serde_json::from_str(&doc).unwrap();
+        let _: HEPluginInstance<HeTitle> = serde_json::from_str(&doc).unwrap();
     }
 
     #[test]
--- src/plugins.rs
+++ src/plugins.rs
@@ -1,11 +1,53 @@
-use serde::{de, de::Deserializer, Deserialize, Serialize};
+use serde::{de, de::Deserializer, Deserialize, Serialize, Serializer};
 use serlo_he_spec_derive::plugin_spec;
 use serlo_he_spec_meta;
-use uuid::Uuid;
 use std::default::Default;
+use std::fmt;
+use uuid::Uuid;
 
 const _REFRESHER: &str = include_str!("he_plugins.yml");
 plugin_spec!("src/he_plugins.yml");
 
-type MarkdownString = String;
-type TitleString = String;
+#[derive(Debug, Serialize, Deserialize, PartialEq, Hash, Clone)]
+/// Some verified markdown text.
+pub struct MarkdownText(String);
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Hash, Clone)]
+/// Some verified title text.
+pub struct TitleText(String);
+
+impl Default for MarkdownText {
+    fn default() -> Self {
+        Self("use *formatted* text here!".to_string())
+    }
+}
+
+impl Default for TitleText {
+    fn default() -> Self {
+        Self("use plain title text here!".to_string())
+    }
+}
+
+impl fmt::Display for TitleText {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl fmt::Display for MarkdownText {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl MarkdownText {
+    pub fn from_str(text: &str) -> Self {
+        MarkdownText(text.into())
+    }
+}
+
+impl TitleText {
+    pub fn from_str(text: &str) -> Self {
+        TitleText(text.into())
+    }
+}
